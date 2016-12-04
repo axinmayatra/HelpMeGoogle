e7780f37812afb1c62cb25f718a4b7bccd2de0ac
Axin <axinmayatra@gmail.com>
2016-12-04 16:31:42 +0530

    Threading in updating text

--- q.py
+++ q.py
@@ -18,29 +18,36 @@ def search(q):
 
 def getDate(a=1,s=1,d=1):
 	def url():
-		if(E1.get() == ''):
+		if(E1.get() != ''):
+			s = search(E1.get())
+			text = Text(top,height=len(s),width=20)
+			for x in s :
+				text.insert(INSERT,x+'\n')
+			text.grid(row=2,column=1,sticky=W)
 			return
-		s = search(E1.get())
-		text = Text(top,height=len(s),width=20)
-		for x in s :
-			text.insert(INSERT,x+'\n')
-		text.grid(row=2,column=1,sticky=W)
+		else:
+			text = Text(top,height=1,width=20)
+			text.insert(INSERT,"")
+
+			return
 	t = threading.Thread(target=url)
 	t.start()
 
 top = Tk()
-top.title("Centered!")
+top.title("GoogleAtHelp!")
 # center(top)
 
 top.wait_visibility(top)
 top.wm_attributes('-alpha',0.8)
 
 E1 = Entry(top,bd =5)
+text = Text(top,height=0,width=20)
 
 E1.grid(row=1,column=1)
 
 # ee.trace('w',getDate)
 # top.bind('<Return>', getDate)
 E1.bind('<Key>', getDate)
+# E1.after(300,getDate);
 
 top.mainloop()
